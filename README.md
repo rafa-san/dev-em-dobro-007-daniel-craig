49940c0d3e3cb6c35124c65155d835c814ec187c
Rafael <88090582+rafa-san@users.noreply.github.com>
2023-03-12 15:56:29 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# dev-em-dobro-007-daniel-craig
+ Projeto com mentoria do canal do Youtube Dev em Dobro.
---
+++ js/principal.js
@@ -0,0 +1,51 @@
+/*
+OBJETIVO: Quando clicarmos no botão, temos que mostrar a imagem de fundo e o texto correspondente.
+
+Passo 1: Dar um jeito de pegar o elemento HTML dos botões e das imagens.
+
+Passo 2: Dar um jeito de identificar o clique do usuário no botão.
+
+Passo 3: Desmarcar o botão selecionado anterior.
+
+Passo 4: Marcar o botão clicado como se estivesse selecionado.
+
+Passo 5: Esconder a imagem ativa e o texto ativo anterior.
+
+Passo 6: Fazer aparecer a imagem e o texto correspondente ao botão clicado.
+*/
+
+// Passo 1: Dar um jeito de pegar o elemento HTML dos botões e das imagens.
+const botoesCarrossel = document.querySelectorAll('.botao');
+const imagens = document.querySelectorAll('.imagem'); // foi colocado no passo 6.
+const titulos = document.querySelectorAll('.titulo-filme');
+const paragrafos = document.querySelectorAll('.sinopse');
+
+// Passo 2: Dar um jeito de identificar o clique do usuário no botão
+botoesCarrossel.forEach((botao, indice) => { // o indice foi colocado no passo 6.
+  botao.addEventListener('click', () => {
+    
+    // Passo 3: Desmarcar o botão selecionado anterior.
+    const botaoSelecionado = document.querySelector('.selecionado');
+    botaoSelecionado.classList.remove('selecionado');
+
+    // Passo 4: Marcar o botão clicado como se estivesse selecionado.
+    botao.classList.add('selecionado');
+
+    // Passo 5: Esconder a imagem ativa e o texto ativo anterior.
+    const imagemAtiva = document.querySelector('.ativa');
+    imagemAtiva.classList.remove('ativa');
+
+    const tituloAtivo = document.querySelector('.ativo');
+    tituloAtivo.classList.remove('ativo');
+
+    const sinopseAtiva = document.querySelector('.sinopse-ativa');
+    sinopseAtiva.classList.remove('sinopse-ativa');
+    
+
+    // Passo 6: Fazer aparecer a imagem e o texto correspondente ao botão clicado.
+    imagens[indice].classList.add('ativa');
+    titulos[indice].classList.add('ativo');
+    paragrafos[indice].classList.add('sinopse-ativa')
+    
+  })
+})
